5c919fe10611106548f85c2affa206f1207ef24b
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-11-07 22:28:27 +0600

    file setup and footer done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Router";
+
 function App() {
-  return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h3>LOgin</h3>
+    </div>
+  );
+};
+
+export default Login;
